7eb8ba4d3a2b89587177ae8db040b09486ec3e3c
Giadagenovesi <giada.g@outlook.it>
2023-04-12 10:02:34 +0200

    Correct exercise, completed basic exercise

--- js/script.js
+++ js/script.js
@@ -8,13 +8,15 @@ createApp({
     },
     methods: {
         generateEmails() {
-            axios
+            for (let i = 0; i < 10; i++) {
+                axios
             .get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then((resp) => {
                 if(this.emails.length < 10){
                     this.emails.push(resp.data.response);
                 }
             })
+            } 
         }
     }
 }).mount("#app")
